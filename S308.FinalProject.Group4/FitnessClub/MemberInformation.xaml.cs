e3667135a20a72b18fcd4731a242ef238c8a3a05
wgalvin1 <willyglvin@gmail.com>
2018-04-04 18:40:39 -0400

    made some changes on memberinfo

--- S308.FinalProject.Group4/FitnessClub/MemberInformation.xaml.cs
+++ S308.FinalProject.Group4/FitnessClub/MemberInformation.xaml.cs
@@ -22,6 +22,18 @@ namespace FitnessClub
         public MemberInformation()
         {
             InitializeComponent();
+            
+            
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "Membership Type:".PadRight(12) + "Gold" + Environment.NewLine +
+                "StartDate:".PadRight(12) + "01/01/2018" + Environment.NewLine + "EndDate:".PadRight(12) + "01/01/2019" + Environment.NewLine
+                + "Membership Cost:".PadRight(12) + "$35" + Environment.NewLine
+                + "Subtotal:".PadRight(12) + "$420" + Environment.NewLine
+                + "Additional Features:".PadRight(12) + Environment.NewLine + "Personal Training Plan".PadRight(12).PadLeft(5) + "$200" + Environment.NewLine
+                + "Total:".PadRight(12) + "$620" + Environment.NewLine;
         }
     }
 }
