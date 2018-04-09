766da65f71647591a26ed85c665f1b16273897cb
kelvinchongcf <chongch@iu.edu>
2018-04-09 09:13:09 -0400

    finalfinal

--- S308.FinalProject.Group4/FitnessClub/MainMenu.xaml.cs
+++ S308.FinalProject.Group4/FitnessClub/MainMenu.xaml.cs
@@ -20,7 +20,8 @@ namespace FitnessClub
     public partial class MainMenu : Window
     {
         public MainMenu()
-            //Kelvin Chong, Willy Galvin, Sanyam Gupta
+        //Kelvin Chong, Willy Galvin, Sanyam Gupta
+        //Logo took from Google Images https://www.google.com/search?tbm=isch&source=hp&biw=1536&bih=758&ei=w2bLWsGoCMXTjwSx85HACg&q=fitness+logo&oq=fitness+logo&gs_l=img.3..35i39k1j0l9.1986.3317.0.3488.14.14.0.0.0.0.98.761.12.12.0....0...1ac.1.64.img..2.12.760.0...0.LfmbwjvM2bQ#imgrc=yy8lSf_eskyi9M:
         {
             InitializeComponent();
         }
